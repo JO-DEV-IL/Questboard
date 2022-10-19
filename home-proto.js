ceebf4a3260107f8ed2f5eadf7c28f8e90822fea
JO-DEV-IL <jillianokeefe.dev@gmail.com>
2022-10-19 14:50:21 -0400

    Update home-proto.js

--- home-proto.js
+++ home-proto.js
@@ -1,6 +1,6 @@
 //If there are any quests in localStorage, grab them
 window.addEventListener('load', () => {
     quests = JSON.parse(localStorage.getItems('quests')) || []
-    const nameInput = document.querySelector('name')
+    const newQuestForm = document.querySelector('new-quest-form')
     const questList = document.querySelector('quest-list')
 })
